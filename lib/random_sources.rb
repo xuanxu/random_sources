167176d115e46447a7a8cb206de7a7f91092d815
Juanjo Bazán <jjbazan@gmail.com>
2010-08-11 22:10:52 +0200

    RandomSources module

---
+++ lib/random_sources.rb
@@ -0,0 +1,14 @@
+require 'open-uri'
+
+module RandomSources
+  
+  def self.list
+    self.constants*', '
+  end  
+  
+  def self.generator(provider)
+    return self.const_get(provider).new if self.constants.include?(provider.to_sym)
+    raise NameError.new("Provider not available or name not recognized. Check the list of supported providers with RandomSources.list", provider.to_s) 
+  end
+
+end
