c31c81fbd20ce9a64be217b7a6f05a495d4fcc41
Juanjo Bazán <jjbazan@gmail.com>
2016-01-02 22:16:22 +0100

    favors new hash syntax

--- lib/providers/random_org.rb
+++ lib/providers/random_org.rb
@@ -1,5 +1,5 @@
 module RandomSources
-  
+
   # The RandomOrg class uses the http service provided by http://www.random.org.
   #
   # RANDOM.ORG offers numbers with randomness coming from atmospheric noise.
@@ -9,19 +9,19 @@ module RandomSources
   # Use the service properly and check the website if you need more details about the terms of usage.
   class RandomOrg
     attr_reader :website
-    
+
     def initialize
       @website = "http://www.random.org/"
     end
-    
+
     # Short info about the online source.
     def info
       "RANDOM.ORG offers numbers with randomness coming from atmospheric noise.
       The service was built and is being operated by Mads Haahr of the School of Computer Science and Statistics at Trinity College, Dublin in Ireland."
     end
-  
+
     # Random Integers Generator.
-    # 
+    #
     # Configuration options:
     # * <tt>:num</tt> - The number of integers requested. Posibles values: [1,1e4]. Default: 10
     # * <tt>:min</tt> - The smallest value allowed for each integer. Posibles values: [-1e9,1e9]. Default: 1
@@ -30,33 +30,33 @@ module RandomSources
     #
     # It returns an Array of integers with the size indicated by <tt>:num</tt>
     #
-    #   integers(:num => 15, :max => 2, :min => 200, base => 8)  #=> array of 15 base 8 numbers between 2 and 200
-    #   integers(:num => 4, :max => 33)                          #=> [31, 25, 28, 6]
+    #   integers(num: 15, max: 2, min: 200, base: 8)  #=> array of 15 base 8 numbers between 2 and 200
+    #   integers(num: 4, max: 33)                     #=> [31, 25, 28, 6]
     #
     def integers(options = {})
-      url_params = { :max => clean(options[:max]) || 100, 
-                     :min => clean(options[:min]) || 1, 
-                     :num => clean(options[:num]) || 10, 
-                     :base => clean(options[:base]) || 10, 
-                     :rnd => 'new',
-                     :format => 'plain',
-                     :col => 1
+      url_params = { max: clean(options[:max]) || 100,
+                     min: clean(options[:min]) || 1,
+                     num: clean(options[:num]) || 10,
+                     base: clean(options[:base]) || 10,
+                     rnd: 'new',
+                     format: 'plain',
+                     col: 1
                    }
 
       numbers=[]
 
       check_for_http_errors{
-        response=open("#{@website}integers/?max=#{url_params[:max]}&min=#{url_params[:min]}&base=#{url_params[:base]}&col=#{url_params[:col]}&rnd=#{url_params[:rnd]}&format=#{url_params[:format]}&num=#{url_params[:num]}") 
+        response=open("#{@website}integers/?max=#{url_params[:max]}&min=#{url_params[:min]}&base=#{url_params[:base]}&col=#{url_params[:col]}&rnd=#{url_params[:rnd]}&format=#{url_params[:format]}&num=#{url_params[:num]}")
         response.each_line{|line| numbers << line.to_i}
       }
-      
+
       numbers
     end
-    
+
      # The Sequence Generator
-     # 
+     #
      # It will randomize a given interval of integers, i.e., arrange them in random order.
-     # 
+     #
      # It needs two params:
      # * <tt>min</tt> - The lower bound of the interval (inclusive). Posibles values: [-1e9,1e9]
      # * <tt>max</tt> - The upper bound of the interval (inclusive). Posibles values: [-1e9,1e9]
@@ -64,14 +64,14 @@ module RandomSources
      # The length of the sequence (the largest minus the smallest value plus 1) can be no greater than 10,000.
      #
      # It returns an Array of all the integers of the given interval arranged randomly
-     # 
+     #
      #   sequence(2, 15)  #=> [13, 2, 10, 4, 9, 15 ,12, 3, 5, 7, 6, 14, 8, 11]
     def sequence(min, max)
-      url_params = { :max => clean(max) || 10, 
-                     :min => clean(min) || 1, 
-                     :rnd => 'new',
-                     :format => 'plain',
-                     :col => 1
+      url_params = { max: clean(max) || 10,
+                     min: clean(min) || 1,
+                     rnd: 'new',
+                     format: 'plain',
+                     col: 1
                    }
       sequence_numbers=[]
 
@@ -82,9 +82,9 @@ module RandomSources
 
       sequence_numbers
     end
-    
+
     # Random Strings Generator.
-    # 
+    #
     # It will generate truly random strings of various length and character compositions.
     #
     # Configuration options:
@@ -93,76 +93,76 @@ module RandomSources
     # * <tt>:digits</tt> - Determines whether digits (0-9) are allowed to occur in the strings. Posibles values: ['on', 'off']. Default: on
     # * <tt>:upperalpha</tt> - Determines whether uppercase alphabetic characters (A-Z) are allowed to occur in the strings. Posibles values: ['on', 'off']. Default: on
     # * <tt>:loweralpha</tt> - Determines lowercase alphabetic characters (a-z) are allowed to occur in the strings. Posibles values: ['on', 'off']. Default: on
-    # * <tt>:unique</tt> - Determines whether the strings picked should be unique (as a series of raffle tickets drawn from a hat) or not (as a series of die rolls). 
-    # If unique is set to on, then there is the additional constraint that the number of strings requested (num) must be less than or equal to the number of strings 
+    # * <tt>:unique</tt> - Determines whether the strings picked should be unique (as a series of raffle tickets drawn from a hat) or not (as a series of die rolls).
+    # If unique is set to on, then there is the additional constraint that the number of strings requested (num) must be less than or equal to the number of strings
     # that exist with the selected length and characters. Posibles values: ['on', 'off']. Default: on
-    # 
+    #
     # It returns an Array of Strings of the size indicated with <tt>:num</tt>
-    # 
-    #   strings(:num => 15, :len => 2, :digits => 'off', upperalpha => 'off')  #=> array of 15 strings of size 2 composed by diggits and lowercase letters with no repetition.
-    #   strings(:num => 4, :len => 10)                                         #=> ["iloqQz2nGa", "D2mgs12kD6", "yMe1eDsinJ", "ZQPaEol6xr"]
+    #
+    #   strings(num: 15, len: 2, digits: 'off', upperalpha: 'off')  #=> array of 15 strings of size 2 composed by diggits and lowercase letters with no repetition.
+    #   strings(num: 4, len: 10)                                    #=> ["iloqQz2nGa", "D2mgs12kD6", "yMe1eDsinJ", "ZQPaEol6xr"]
     def strings(options = {})
-      url_params = { :num => clean(options[:num]) || 10,
-                     :len => clean(options[:len]) || 8, 
-                     :digits => check_on_off(options[:digits]) || 'on',
-                     :unique => check_on_off(options[:unique]) || 'on',
-                     :upperalpha => check_on_off(options[:upperalpha]) || 'on',
-                     :loweralpha => check_on_off(options[:loweralpha]) || 'on',
-                     :rnd => 'new',
-                     :format => 'plain'
+      url_params = { num: clean(options[:num]) || 10,
+                     len: clean(options[:len]) || 8,
+                     digits: check_on_off(options[:digits]) || 'on',
+                     unique: check_on_off(options[:unique]) || 'on',
+                     upperalpha: check_on_off(options[:upperalpha]) || 'on',
+                     loweralpha: check_on_off(options[:loweralpha]) || 'on',
+                     rnd: 'new',
+                     format: 'plain'
                    }
 
       strings=[]
 
       check_for_http_errors{
-        response=open("#{@website}strings/?num=#{url_params[:num]}&len=#{url_params[:len]}&digits=#{url_params[:digits]}&unique=#{url_params[:unique]}&upperalpha=#{url_params[:upperalpha]}&loweralpha=#{url_params[:loweralpha]}&rnd=#{url_params[:rnd]}&format=#{url_params[:format]}") 
+        response=open("#{@website}strings/?num=#{url_params[:num]}&len=#{url_params[:len]}&digits=#{url_params[:digits]}&unique=#{url_params[:unique]}&upperalpha=#{url_params[:upperalpha]}&loweralpha=#{url_params[:loweralpha]}&rnd=#{url_params[:rnd]}&format=#{url_params[:format]}")
         response.each_line{|line| strings << line.strip}
       }
-      
+
       strings
     end
-    
+
     # The Quota Checker.
     #
-    # This method allows you to examine your quota at any point in time. 
-    # The quota system of random.org works on the basis of IP addresses. 
+    # This method allows you to examine your quota at any point in time.
+    # The quota system of random.org works on the basis of IP addresses.
     # Each IP address has a base quota of 1,000,000 daily bits.
     #
-    # If your quota is greater than or equal to zero, the next request for random numbers will be fully completed, even if the request will result in the quota becoming negative. 
+    # If your quota is greater than or equal to zero, the next request for random numbers will be fully completed, even if the request will result in the quota becoming negative.
     # Hence, no partial responses will be sent as a result of exhausting ehe quota; the server will either return a full response or an error response.
     #
-    # Every day, shortly after midnight UTC, all quotas with less than 1,000,000 bits receive a free top-up of 200,000 bits. 
+    # Every day, shortly after midnight UTC, all quotas with less than 1,000,000 bits receive a free top-up of 200,000 bits.
     # If the server has spare capacity, you may get an additional free top-up earlier, but you should not count on it.
     # If you need extra bits urgently (or require many bits) you can purchase once-off top-ups from the Quota Page: http://www.random.org/quota.
     #
     # The method returns the number of bits left of your quota as an integer.
     def quota
-      url_params = { :format => 'plain' }
+      url_params = { format: 'plain' }
       check_for_http_errors{
-       response=open("#{@website}quota/?format=#{url_params[:format]}") 
+       response=open("#{@website}quota/?format=#{url_params[:format]}")
        return response.to_i
       }
     end
 
-    private 
+    private
 
     def clean(p) #:nodoc:
       (p.nil? || p.to_s=='') ? nil : p.to_i
     end
-    
+
     def check_on_off(p) #:nodoc:
       return nil if p.nil?
-      ['ON', 'OFF'].include?(p.upcase) ? p : nil 
-    end  
-    
+      ['ON', 'OFF'].include?(p.upcase) ? p : nil
+    end
+
     def check_for_http_errors #:nodoc:
       begin
         yield
-      rescue OpenURI::HTTPError => boom 
-       raise StandardError.new("Error from server: "+boom.io.read.strip)   
+      rescue OpenURI::HTTPError => boom
+       raise StandardError.new("Error from server: "+boom.io.read.strip)
       end
     end
-    
+
   end
-  
+
 end
--- spec/random_org_spec.rb
+++ spec/random_org_spec.rb
@@ -1,112 +1,112 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
 describe RandomSources::RandomOrg do
-  
+
   describe 'Integers generator' do
 
     before(:each) do
       @base_url = "http://www.random.org/integers/"
-      @default_params = {'max' => 100, 
-                         'min' => 1, 
-                         'base' => 10, 
-                         'col' => 1, 
-                         'rnd' => 'new', 
-                         'format' => 'plain', 
+      @default_params = {'max' => 100,
+                         'min' => 1,
+                         'base' => 10,
+                         'col' => 1,
+                         'rnd' => 'new',
+                         'format' => 'plain',
                          'num' => 10 }
       @default_query = get_url(@base_url, @default_params)
       @default_response = "4\n 8\n 15\n 16\n 23\n 33\n 42\n"
       @generator = RandomSources::RandomOrg.new
       @generator.stub!(:open).and_return(@default_response)
     end
-  
+
     it "hits random_org url with default query if no options provided" do
       @generator.should_receive(:open).with(@default_query).and_return(@default_response)
       @generator.integers
     end
-    
+
     it "hits random_org url with provided options" do
       @generator.should_receive(:open).with( get_url(@base_url,
                                              @default_params.merge({'max'=>50, 'min'=>3, 'num'=>4})) ).and_return(@default_response)
-      @generator.integers(:max => 50, :min => 3, :num => 4)
+      @generator.integers(max: 50, min: 3, num: 4)
     end
-    
+
     it "returns an Array of integers" do
       @generator.integers.class.should == Array
       @generator.integers.size.should == 7
       @generator.integers.first.should == 4
     end
-    
+
     it "raises an exception with the message from the server in case of a http error response" do
       exc = OpenURI::HTTPError.new("Error 503", StringIO.new("Wrong query params"))
       @generator.stub!(:open).and_raise(exc)
-      lambda{@generator.integers(:max=>-12000000)}.should raise_exception{|e| e.message.should == "Error from server: Wrong query params"}
+      lambda{@generator.integers(max: -12000000)}.should raise_exception{|e| e.message.should == "Error from server: Wrong query params"}
     end
-    
+
     it "do not let user modify format, col or rnd query params" do
       @generator.should_receive(:open).with( get_url(@base_url,
                                              @default_params.merge({'max'=>50, 'min'=>3, 'num'=>4})) ).and_return(@default_response)
-      @generator.integers(:max => 50, :min => 3, :num => 4, :col => 4, :rnd => 'changed', :format => 'html')
+      @generator.integers(max: 50, min: 3, num: 4, col: 4, rnd: 'changed', format: 'html')
     end
-    
+
     it "is protected against sql injection" do
       @generator.should_receive(:open).with(@default_query).and_return(@default_response)
-      @generator.integers(:max => "100' OR 1=1")
+      @generator.integers(max: "100' OR 1=1")
     end
   end
-  
-  describe 'Sequence generator' do 
+
+  describe 'Sequence generator' do
     before(:each) do
       @base_url = "http://www.random.org/sequences/"
-      @default_params = {'max' => 10, 
-                         'min' => 1, 
-                         'col' => 1, 
-                         'rnd' => 'new', 
+      @default_params = {'max' => 10,
+                         'min' => 1,
+                         'col' => 1,
+                         'rnd' => 'new',
                          'format' => 'plain'}
       @default_query = get_url(@base_url, @default_params)
       @default_response = "8\n 10\n 4\n 1\n 6\n 2\n 3\n 9\n 5\n 7\n"
       @generator = RandomSources::RandomOrg.new
       @generator.stub!(:open).and_return(@default_response)
     end
-    
+
     it "hits random_org url with default query if no options provided" do
       @generator.should_receive(:open).with(@default_query).and_return(@default_response)
       @generator.sequence(1, 10)
     end
-    
+
     it "hits random_org url with provided options" do
       @generator.should_receive(:open).with(get_url(@base_url,
                                              @default_params.merge({'max'=>33, 'min'=>21})) ).and_return(@default_response)
       @generator.sequence(21, 33)
     end
-    
+
     it "returns an Array of integers" do
       @generator.sequence(1, 10).class.should == Array
       @generator.sequence(1, 10).size.should == 10
       @generator.sequence(1, 10).first.should == 8
     end
-    
+
     it "raises an exception with the message from the server in case of a http error response" do
       exc = OpenURI::HTTPError.new("Error 503", StringIO.new("Wrong query params"))
       @generator.stub!(:open).and_raise(exc)
-      lambda{@generator.sequence(0,-13000000)}.should raise_exception{|e| e.message.should == "Error from server: Wrong query params"}
+      lambda{@generator.sequence(0, -13000000)}.should raise_exception{|e| e.message.should == "Error from server: Wrong query params"}
     end
-    
+
     it "is protected against sql injection" do
       @generator.should_receive(:open).with(@default_query).and_return(@default_response)
       @generator.sequence(1, "10\" OR 1=1")
     end
   end
-  
+
   describe 'String generator' do
     before(:each) do
       @base_url = "http://www.random.org/strings/"
       @default_params = {'num' => 10 ,
-                         'len' => 8, 
-                         'digits' => 'on', 
-                         'unique' => 'on', 
+                         'len' => 8,
+                         'digits' => 'on',
+                         'unique' => 'on',
                          'upperalpha' => 'on',
                          'loweralpha' => 'on',
-                         'rnd' => 'new', 
+                         'rnd' => 'new',
                          'format' => 'plain'}
       @default_query = get_url(@base_url, @default_params)
       @default_response = "mfu0G89z\n azYZw48F\n qmcOUqIj\n TGytf0Hs\n 6f1jaYDC\n ExPLnE6N\n cWS3JoYW\n IGw2sCqm\n KpSsixvn\n Hw8F6oQC"
@@ -118,39 +118,39 @@ describe RandomSources::RandomOrg do
       @generator.should_receive(:open).with(@default_query).and_return(@default_response)
       @generator.strings
     end
-    
+
     it "hits random_org url with provided options" do
       @generator.should_receive(:open).with( get_url(@base_url,
                                              @default_params.merge({'len'=>50, 'digits'=>'off', 'num'=>7, 'unique'=>'off'})) ).and_return(@default_response)
-      @generator.strings(:len => 50, :digits => 'off', :num => 7, :unique => 'off')
+      @generator.strings(len: 50, digits: 'off', num: 7, unique: 'off')
     end
-    
+
     it "returns an Array of strings" do
       @generator.strings.class.should == Array
       @generator.strings.size.should == 10
       @generator.strings.first.should == "mfu0G89z"
       @generator.strings.last.should == "Hw8F6oQC"
     end
-    
+
     it "raises an exception with the message from the server in case of a http error response" do
       exc = OpenURI::HTTPError.new("Error 503", StringIO.new("Wrong query params"))
       @generator.stub!(:open).and_raise(exc)
-      lambda{@generator.strings(:len => -40)}.should raise_exception{|e| e.message.should == "Error from server: Wrong query params"}
+      lambda{@generator.strings(len: -40)}.should raise_exception{|e| e.message.should == "Error from server: Wrong query params"}
     end
-    
+
     it "do not let user modify format or rnd query params" do
       @generator.should_receive(:open).with( get_url(@base_url,
                                              @default_params.merge({'len'=>50, 'num'=>4, 'unique'=>'off', 'digits' => 'off'})) ).and_return(@default_response)
-      @generator.strings(:len => 50, :digits => 'off', :num => 4, :unique => 'off', :rnd => 'changed', :format => 'html')
+      @generator.strings(len: 50, digits: 'off', num: 4, unique: 'off', rnd: 'changed', format: 'html')
     end
-    
+
     it "is protected against sql injection" do
       @generator.should_receive(:open).with(@default_query).and_return(@default_response)
-      @generator.strings(:len => "8' OR 1=1")
+      @generator.strings(len: "8' OR 1=1")
     end
-    
+
   end
-  
+
   describe 'Quota checker' do
     before(:each) do
       @base_url = "http://www.random.org/quota/"
@@ -160,22 +160,22 @@ describe RandomSources::RandomOrg do
       @generator = RandomSources::RandomOrg.new
       @generator.stub!(:open).and_return(@default_response)
     end
-    
+
     it "hits random_org quota checker url" do
       @generator.should_receive(:open).with(@default_query).and_return(@default_response)
       @generator.quota
     end
-    
+
     it "returns the number of bits available" do
       @generator.quota.should be_kind_of Integer
     end
-    
+
     it "raises an exception with the message from the server in case of a http error response" do
       exc = OpenURI::HTTPError.new("Error 503", StringIO.new("Quota checker disabled for maintenance"))
       @generator.stub!(:open).and_raise(exc)
       lambda{@generator.quota}.should raise_exception{|e| e.message.should == "Error from server: Quota checker disabled for maintenance"}
     end
   end
-    
-  
+
+
 end
